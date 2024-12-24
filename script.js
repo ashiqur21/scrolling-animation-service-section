5bebe98a57301461f3dbe1de39668cd92cbfa586
ashiqur rahman biplop <biplops22@gmail.com>
2024-12-24 15:09:13 +0600

    added responsive ness

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 document.addEventListener("scroll", () => {
   const cards = document.querySelectorAll(".explore-inner-card");
   const triggerHeight = window.innerHeight * 0.75;
-
+  const screenSize = window.innerWidth;
   cards.forEach((card) => {
     const cardPosition = card.getBoundingClientRect().top;
-    if (cardPosition < triggerHeight) {
+    if (cardPosition < triggerHeight && screenSize >= 468) {
       card.classList.add("visible");
     } else {
       card.classList.remove("visible");
